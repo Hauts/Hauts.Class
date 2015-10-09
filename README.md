2c480cddc3e6c7697531864a7b4c0d6fd5b25bd1
Hauts <inflashwetrust@yandex.ru>
2015-10-09 11:35:19 +0600

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,240 @@
 # Hauts.Class
+\o
+
+Please contact me in case you need some help via twitter http://twitter.com/inflashwetrust.
+
+Note: code is NOT complete and need lots of optimisations and tests.
+
+## Quick alias
+```javascript
+var Class = Hauts.Class;
+```
+
+## Creating class:
+```javascript
+// Unnamed class without props & methods
+var myClass = Class(function(){
+	// constructor
+});
+var instance = new myClass();
+```
+```javascript
+// Unnamed class with props & methods
+var myClass = Class(function(){
+	// constructor
+}, {
+	// props
+});
+var instance = new myClass();
+```
+```javascript
+// Named class with props & methods
+var myClass = Class('ClassName', function(){
+	// constructor
+}, {
+	// props
+});
+var instance = new myClass();
+```
+
+## Creating class example:
+```javascript
+var Animal = Class('Animal', function (name){
+	this.name = name;
+}, {
+	sayHello: function(){
+		console.log('Animal ' + this.name + ' says hello!');
+	}
+});
+
+var myAnimal = new Animal('Mikky');
+myAnimal.sayHello();
+
+// Animal Mikky says hello!
+```
+
+## Class inheritance:
+```javascript
+// Simply unnamed SubClass extends unnamed BaseClass
+var BaseClass = Class(function(){
+	// BaseClass constructor
+});
+var SubClass = BaseClass.extend(function(){
+	// SubClass constructor
+});
+
+var instance = new SubClass();
+```
+```javascript
+// Named classes inheritance
+var BaseClass = Class('BaseClass', function(){
+	// BaseClass constructor
+});
+var SubClass = BaseClass.extend('SubClass', function(){
+	// SubClass constructor
+});
+
+var instance = new SubClass();
+```
+```javascript
+// Alternative way using Class.extendClass method
+var BaseClass = Class('BaseClass', function(){
+	// BaseClass constructor
+});
+var SubClass = Class.extendClass(BaseClass, function(){
+	// SubClass constructor
+});
+
+var instance = new SubClass();
+```
+```javascript
+// Also may extend 'hand-made' classes
+function SomeFunction(){}
+SomeFunction.prototype.someProperty = 123;
+
+var SubClass = Class.extendClass(SomeFunction, function(){
+	// SubClass constructor
+});
+
+var instance = new SubClass();
+console.log(instance.someProperty);
+
+// 123
+```
+
+## Working with .superClass:
+```javascript
+var BaseClass = Class(function(){
+	this.someProperty = 1;
+});
+var SubClass = BaseClass.extend(function(){
+	this.superClass();
+	console.log(this.someProperty);
+});
+
+var instance = new SubClass();
+
+// 1
+```
+```javascript
+var BaseClass = Class(function (value){
+	this.someProperty = value;
+});
+var SubClass = BaseClass.extend(function(){
+	this.superClass(10);
+	console.log(this.someProperty);
+});
+
+var instance = new SubClass();
+
+// 10
+```
+```javascript
+var BaseClass = Class(function(){}, {
+	test: function(){
+		this.someProperty = 100;
+	}
+});
+var SubClass = BaseClass.extend(function(){
+	this.superClass.test();
+	console.log(this.someProperty);
+});
+
+var instance = new SubClass();
+
+// 100
+```
+```javascript
+var A = Class(function(){}, {
+	test: function(){
+		console.log('Class A test');
+	}
+});
+var B = A.extend(function(){}, {
+	test: function(){
+		this.superClass.test();
+		console.log('Class B test');
+	}
+});
+var C = B.extend(function(){}, {
+	test: function(){
+		this.superClass.test();
+		console.log('Class C test');
+	}
+});
+var D = C.extend(function(){}, {
+	test: function(){
+		this.superClass.test();
+		console.log('Class D test');
+	}
+});
+var E = D.extend(function(){}, {
+	test: function(){
+		this.superClass.test();
+		console.log('Class E test');
+	}
+});
+
+var instance = new E();
+instance.test();
+
+// Class A test
+// Class B test
+// Class C test
+// Class D test
+// Class E test
+```
+
+## Working with instanceof:
+```javascript
+var A = Class(function(){});
+var B = A.extend(function(){});
+
+var instance = new B();
+console.log(instance instanceof A);
+console.log(instance instanceof B);
+
+// true
+// true
+```
+```javascript
+var A = Class(function(){});
+var B = A.extend(function(){});
+var C = B.extend(function(){});
+
+var instance = new C();
+console.log(instance instanceof A);
+console.log(instance instanceof B);
+console.log(instance instanceof C);
+
+// true
+// true
+// true
+```
+## Working with instanceof and 'hand-made' classes:
+```javascript
+var A = function(){}
+var B = Class.extendClass(A, function(){});
+
+var instance = new B();
+console.log(instance instanceof A);
+console.log(instance instanceof B);
+
+// false
+// true
+```
+Forcing instanceof for 'hand-made' classes will launch constructors:
+```javascript
+var A = function(){
+	console.log('A constructor');
+}
+var B = Class.extendClass(A, function(){}, null, true);
+
+var instance = new B();
+console.log(instance instanceof A);
+console.log(instance instanceof B);
+
+// A constructor
+// true
+// true
+```
---
+++ hauts.class.js
@@ -0,0 +1,266 @@
+﻿// Hauts.Class
+// 09.10.2015
+// Hauts
+// https://github.com/Hauts/Hauts.Class
+;(function( factory ){
+	factory.Hauts = factory.Hauts || new (function Hauts(){})();
+	var Hauts = factory.Hauts;
+
+	// Short strings
+	var FN = 'function',
+		UN = 'undefined',
+		CN = 'className',
+		EX = 'extend',
+		IO = 'instanceOf',
+		SC = 'superClass',
+		OB = 'object',
+		RT = 'return',
+		ST = 'string',
+		TH = 'this',
+
+		CLASS_NAME_PREFIX = 'Class_',
+
+		EMPTY = {},
+		DUMMY = Object,
+
+		// Inner settings
+		USE_DEEP_MODE = true,
+		USE_EVAL = true,
+
+		CLASS_COUNTER = 0,
+		INNER_INITIALIZATION = false
+
+	var Class = function(className, classConstructor, classPrototype, staticProperties, useDeepMode){
+		return Class.create( className, classConstructor, classPrototype, staticProperties, useDeepMode );
+	}
+	Class.create = function(className, classConstructor, classPrototype, staticProperties, useDeepMode){
+		return Class.extend( null, className, classConstructor, classPrototype, staticProperties, useDeepMode );
+	}
+
+	Class.useDeepMode = function( state ){
+		if(typeof state != UN){ USE_DEEP_MODE = !!state; }
+		return USE_DEEP_MODE;
+	}
+
+	// Internal helpers
+	function copy( from, to ){ for(var i in from){ to[i] = from[i]; } }
+	function testReserved( name ){ return name==SC || name==EX || name==IO || name==CN; }
+	function createName(){ return CLASS_NAME_PREFIX + (++CLASS_COUNTER); }
+	function createClassStructure( instance ){
+		var structure = [];
+		function fillStructure( owner ){
+			if(owner.superClass && owner.superClass !== DUMMY ){
+				structure.push(owner.superClass);
+				fillStructure(owner.superClass);
+			}
+		}				
+		fillStructure( instance );
+		structure.reverse();
+		structure.push( instance );
+		return structure;
+	}
+
+	Class.extend = function( parentClass, className, classConstructor, classPrototype, staticProperties, useDeepMode ){
+
+		parentClass = typeof parentClass != FN ? DUMMY : parentClass;
+
+		if(typeof className == FN){
+			staticProperties = classPrototype;
+			classPrototype = classConstructor;
+			classConstructor = className;
+			className = createName();
+
+		} else if(typeof className == ST){
+			if(typeof classConstructor != FN){
+				staticProperties = classPrototype;
+				classPrototype = classConstructor;
+				classConstructor = DUMMY;
+			}
+		} else if(typeof className == OB){
+			staticProperties = classPrototype;
+			classPrototype = className;
+			classConstructor = DUMMY;//function(){};
+			className = createName();
+
+		} else if( typeof className == UN){
+			className = createName();
+		}
+
+		if(typeof classConstructor == UN){
+			classConstructor = DUMMY;
+		} else {
+			if(typeof classConstructor != FN){
+				throw new Error('classConstructor must be a Function');
+			}
+		}
+		classPrototype = typeof classPrototype == UN ? EMPTY : classPrototype;
+		staticProperties = typeof staticProperties == UN ? EMPTY : staticProperties;
+
+		var parentClassPrototype = parentClass.prototype;
+
+		// superClass magic starts here
+		var superClassInAction = false;
+
+		function createSuperClass( instance, createSuperClassTo, parentClass ){
+			var superClassCalled = false;
+			var superClass = function(){
+				if(!superClassCalled){
+					superClassCalled = true;
+					var origSuperClass = instance.superClass;
+					instance.superClass = createSuperClassTo.superClass.superClass;
+						parentClass.apply( instance, arguments );
+					instance.superClass = origSuperClass;
+				}
+				return superClass;
+			}
+			superClass.className = parentClass.className;
+			return superClass;
+		}
+
+		function generateSuperClassProperty( instance ){
+			function wrapMethod( method, replaceSuperClass, methodName ){
+				return function(){
+					superClassInAction = true;
+						var currentSuperClass = instance.superClass;
+						instance.superClass = replaceSuperClass;
+							var result = method.apply( instance, arguments );
+						instance.superClass = currentSuperClass;
+					superClassInAction = false;
+					return result;
+				}
+			}
+			function wrapSuperClassMethods( holder, superClassPrototype, replaceContext ){
+				for(var i in superClassPrototype){
+					if( !testReserved( i ) ){
+						var prop = superClassPrototype[i];
+						if( typeof prop == FN ){
+							holder[i] = wrapMethod( prop, replaceContext, i );
+						}
+					}
+				}
+			}
+			var structure = createClassStructure(instance);
+			for(var k=1, totalStructure = structure.length, superClasses = []; k<totalStructure; k++){
+				superClasses.push(createSuperClass(instance, structure[k], structure[k-1]));
+			}
+			for(var k=1, totalSuperClasses = superClasses.length; k<totalSuperClasses; k++){
+				var superClass = superClasses[k];
+				superClass.superClass = superClasses[k-1];
+			}
+			for(var k=totalSuperClasses-1; k>=0; k--){
+				wrapSuperClassMethods( superClasses[k], structure[k].prototype, superClasses[k-1] );
+			}
+			instance.superClass = superClasses[totalSuperClasses-1];	
+		}
+
+		function wrapInnerClassMethods( instance ){
+			function wrapInnerClassMethod( method, context, baseSuperClass ){
+				return function(){
+					if(superClassInAction){
+						context.superClass = baseSuperClass;
+					}
+					return method.apply(context, arguments);
+				}
+			}			
+			var superClass = instance.superClass;
+			for(var i in instance){
+				if( !testReserved( i ) ){
+					var prop = instance[i];
+					if(typeof prop == FN){
+						instance[i] = wrapInnerClassMethod( prop, instance, superClass );
+					}
+				}
+			}
+		}
+
+		// Create classWrapper
+		var classWrapper = function(){
+			if(!INNER_INITIALIZATION){
+				generateSuperClassProperty( this );
+				classConstructor.apply( this, arguments );
+				wrapInnerClassMethods( this );
+				return this;
+			}
+		}
+		if(USE_EVAL){
+			var classWrapperCodeArray = classWrapper.toString().split(FN);
+			var classWrapperCode = classWrapperCodeArray.shift() + FN + ' ' + className + classWrapperCodeArray.join(FN);
+				classWrapperCode = '(' + FN + '(){' + RT + ' ' + classWrapperCode + '})()';
+			try{
+				classWrapper = eval(classWrapperCode);
+			} catch(e){
+				console.error('Could not eval class constructor function with source code: \n'+ classWrapperCode);
+			}
+		}
+
+		// Copy parentClass prototype
+		useDeepMode = typeof useDeepMode == UN ? USE_DEEP_MODE : !!useDeepMode;
+		if(USE_DEEP_MODE && useDeepMode){
+			INNER_INITIALIZATION = true;
+				classWrapper.prototype = new parentClass();
+			INNER_INITIALIZATION = false;
+		} else {
+			copy( parentClassPrototype, classWrapper.prototype );
+		}
+
+		// Overwrite with new classPrototype object props
+		copy( classPrototype, classWrapper.prototype )
+
+		for(var i in classWrapper.prototype){
+			if( !testReserved( i ) ){
+				var prop = classWrapper.prototype[i];
+				if(typeof prop == FN){
+					if(!Object.prototype.hasOwnProperty.call(classPrototype,i)){
+						classWrapper.prototype[i] = eval('('+FN+'(){'+RT+' '+FN+'(){'+RT+' '+TH+'.'+SC+'.'+i+'.apply('+TH+',arguments);}})()');
+					}
+				}
+			}
+		}
+
+		// Add static properties
+		copy( staticProperties, classWrapper );
+
+		classWrapper.superClass = classWrapper.prototype.superClass = parentClass;
+		classWrapper.className = classWrapper.prototype.className = className;
+
+		classWrapper.extend = classWrapper.prototype.extend = function(className, classConstructor, classPrototype, staticProperties, useDeepMode){
+			return Class.extend(classWrapper, className, classConstructor, classPrototype, staticProperties, useDeepMode);
+		}
+
+		classWrapper.instanceOf = classWrapper.prototype.instanceOf = function( testClass ){
+			if(classWrapper === testClass){
+				return true;
+			}
+			if(classWrapper.superClass){
+				if(classWrapper.superClass === testClass){
+					return true;
+				} else {
+					if(typeof classWrapper.superClass.instanceOf == FN){
+						return classWrapper.superClass.instanceOf( testClass );
+					}
+				}
+			}
+			return false;
+		}
+
+		if(Object.defineProperty){
+			Object.defineProperty(classWrapper, SC, {writable: false, value: parentClass});
+
+			var classNameProps = {writable: false, value: className};
+				Object.defineProperty(classWrapper, CN, classNameProps);
+				Object.defineProperty(classWrapper.prototype, CN, classNameProps);
+
+			var extendProps = { writable: false, value: classWrapper.extend };
+				Object.defineProperty(classWrapper, EX, extendProps);
+				Object.defineProperty(classWrapper.prototype, EX, extendProps);
+
+			var instanceOfProps = { writable: false, value: classWrapper.instanceOf };
+				Object.defineProperty(classWrapper, IO, instanceOfProps);
+				Object.defineProperty(classWrapper.prototype, IO, instanceOfProps);
+		}
+		return classWrapper;
+	}
+
+	// Exports
+	Hauts.Class = Class;
+})(this);
